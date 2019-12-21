7b94b1070952f97047f45c1ec0f049e80217f381
VadikProg <38704817+VadikProg@users.noreply.github.com>
2019-12-21 21:59:57 +0300

    Add files via upload
    GaySolvesssss

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,28 @@
+import jdk.internal.misc.Unsafe;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import java.io.*;
 import java.util.*;
-
+import java.lang.*;
+import java.lang.reflect.*;
 
 public class Main{
+
+    public static void disableWarning() {
+        try {
+            Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+            theUnsafe.setAccessible(true);
+            Unsafe u = (Unsafe) theUnsafe.get(null);
+
+            Class cls = Class.forName("jdk.internal.module.IllegalAccessLogger");
+            Field logger = cls.getDeclaredField("logger");
+            u.putObjectVolatile(cls, u.staticFieldOffset(logger), null);
+        } catch (Exception e) {
+            // ignore
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
