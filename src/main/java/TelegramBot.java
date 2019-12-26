bc718497c4924d4463d45603ca808044801b40d5
VadikProg <38704817+VadikProg@users.noreply.github.com>
2019-12-26 17:37:27 +0300

    Add files via upload

--- src/main/java/TelegramBot.java
+++ src/main/java/TelegramBot.java
@@ -1,25 +1,34 @@
 import java.lang.*;
 import java.io.*;
 import java.math.*;
+import java.sql.Array;
+import java.util.Arrays;
+
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-public class Bot extends TelegramLongPollingBot {
+public class TelegramBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
-
+        final String prefix = "/";
         String message = update.getMessage().getText();
         SendMessage SendMessage = new SendMessage();
         SendMessage.setChatId(update.getMessage().getChatId().toString());
 
-        if(message.equals("/start")){
-            SendMessage.setText("Привет, я админ этого чата");
-        }else if(message.equals("/BanWords")){
-            SendMessage.setText(update.getMessage().getFrom().getFirstName());
-            SendMessage.setText("Пидор; Нигга; Хач;");
+        if(message.startsWith(prefix)){
+            message = message.substring(prefix.length(), message.length() - 1);
+            String[] Prearguments = message.split(" ");
+            String command = Prearguments[0];
+            String[] arguments = Arrays.copyOfRange(Prearguments, 1, Prearguments.length - 1);
+            if(command.equals("/start")){
+                SendMessage.setText("Привет, я админ этого чата");
+            }else if(command.equals("/BanWords")){
+                SendMessage.setText(update.getMessage().getFrom().getFirstName());
+                SendMessage.setText("Пидор; Нигга; Хач;");
+            }
         }
-
+        
         try {
             execute(SendMessage);
         } catch (TelegramApiException e) {
